2a1477a0b8b880b0c0b7b744c3065e4db24232f3
Dylan Gooch <22dgooch22@gmail.com>
2023-12-07 09:06:23 -0500

    adding sessions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, flash, redirect
+from flask import Flask, request, render_template, flash, redirect, session
 from surveys import satisfaction_survey, personality_quiz, surveys
 
 app = Flask(__name__)
@@ -11,13 +11,21 @@ curr_survey = satisfaction_survey
 def show_home_page():
     return render_template('home.html', title=curr_survey.title, instructions=curr_survey.instructions)
 
+
+@app.route('/session',methods=['POST'])
+def make_session():
+    session['responses'] = []
+    return redirect("/question/0")
+
+
+
 @app.route('/question/<index>')
 def show_question(index):
     i = int(index)
-    if i != len(responses):
+    if i != len(session['responses']):
         flash('Invalid Question')
-        return redirect(f'/question/{len(responses)}')
-    elif len(curr_survey.questions) == len(responses):
+        return redirect(f'/question/{len(session["responses"])}')
+    elif len(curr_survey.questions) == len(session['responses']):
         flash('Survey Already Somplete')
         return redirect('/survey_complete')
     return render_template('question.html', q=curr_survey.questions[i], idx=i)
@@ -30,7 +38,9 @@ def handle_ans():
     print(i,ans,type(ans))
     i = int(i)
     i+=1
-    responses.append(ans)
+    res = session['responses']
+    res.append(ans)
+    session['responses'] = res
     if i < len(curr_survey.questions):
         return redirect(f'/question/{i}')
     return redirect('/survey_complete')
